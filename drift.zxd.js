33478de7ba1d0f6ba4c8fbaab3621eefcd280a8e
张晓东 <zhangxiaodong@zhangxiaodongdeMacBook-Air.local>
2017-03-24 16:06:53 +0800

    上线

--- drift.zxd.js
+++ drift.zxd.js
@@ -1,5 +1,13 @@
 (function($){
 
+
+    function RandomNumBoth(Min,Max){
+          var Range = Max - Min;
+          var Rand = Math.random();
+          var num = Min + Math.round(Rand * Range); //四舍五入
+          return num;
+    }
+
     $.fn.drift=function(options){
         var options;
         var Defults = {
@@ -36,6 +44,8 @@
                     'top':0-(options.maxSize+50)+'px'
                 });
                 var startPositionLeft = Math.random() * documentWidth;
+                var routeEnd = RandomNumBoth(-360,360);
+                // console.log()
                 var sizeFlake = options.minSize + Math.random() * options.maxSize;
                 if(options.wind==0){
                     var endPositionLeft = Math.random() * documentWidth;
@@ -51,11 +61,20 @@
                     "z-index":1001,
                     "opacity": startOpacity,
                     "width": options.maxSize+'px',　//sizeFlake 一大一小把options.maxSize替换成sizeFlake
-                    "color": "#fff"
+                    "color": "#fff",
+                    "-webkit-transform": "rotate("+routeEnd+"deg)",
+                    "-ms-transform":"rotate("+routeEnd+"deg)",
+                    "-o-transform":"rotate("+routeEnd+"deg)",
+                    "transform":"rotate("+routeEnd+"deg)"
+
                 }).animate({
                     "top": documentHeight + 100,
                     "left": endPositionLeft,
-                    "opacity": endOpacity
+                    "opacity": endOpacity,
+                    "-webkit-transform": "rotate("+routeEnd+"deg)",
+                    "-ms-transform":"rotate("+routeEnd+"deg)",
+                    "-o-transform":"rotate("+routeEnd+"deg)",
+                    "transform":"rotate("+routeEnd+"deg)"
                 }, options.sleepAnimate, function () {
                     $(this).remove();
                 });
